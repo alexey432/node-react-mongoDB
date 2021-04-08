f633ef367b91fcf5f31684b69d101d701a3ddd16
alexey432 <alexorl432@gmail.com>
2021-04-08 15:33:34 +0300

    changed

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+    res.send({ bye: 'buddy' });
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
+// "engines": {
+//   "node": "14.16.0",
+//   "npm": "7.6.1"
+// },
