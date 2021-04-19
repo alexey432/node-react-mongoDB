ba977faac71b567fa1ef426e5bc8b5afe57d0bc9
alexey432 <alexorl432@gmail.com>
2021-04-19 13:30:29 +0300

    Payments handling finished

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,10 +1,9 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
-
+const requireLogin = require('../middlewares/requireLogin'); //for checking whether the user logged in or not
 
 module.exports = app => {
-    app.post('/api/stripe', async (req, res) => {
-        // console.log(req.body)
+    app.post('/api/stripe', requireLogin, async (req, res) => {
         const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
@@ -14,7 +13,7 @@ module.exports = app => {
         // Adding credits
         req.user.credits += 5;
         const user = await req.user.save();
+        
         res.send(user)
-        console.log(user)
     });
 };
